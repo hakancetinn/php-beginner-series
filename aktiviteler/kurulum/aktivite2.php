d668db63a143294ea39fad9dfa50a8d8125507c6
Hakan Çetin <hakan_cetin1205@hotmail.com>
2021-02-13 19:23:48 +0300

    Add files via upload

---
+++ aktiviteler/kurulum/aktivite2.php
@@ -0,0 +1,7 @@
+<h2>Tarayıcımda http://localhost:8000 adresine istek yaptığımızı varsayalım. Aşağıdakilerden hangisi doğrudur?</h2>
+<ul style="list-style-type: upper-alpha">
+    <li>URL'de dosya adı (ör. index.php) olmadığı için istek tamamlanamıyor.</li>
+    <li>İstek, uzak bir bilgisayarın IP adresini bulmak için DNS'yi kullanır, 8000 numaralı bağlantı noktasının kapısını çalar ve ardından index.php'yi çalıştırır ve cevap döner.</li>
+    <li>İstek bilgisayarımdaki 8000 numaralı bağlantı noktasından çıkıyor, ancak daha sonra bilgisayarıma geri dönüyor ve index.php'yi çalıştırıyor.</li>
+    <li>İstek yerel bilgisayarımdaki 8000 numaralı bağlantı noktasına geri döner ve index.php çalıştırır ve cevap döner.</li>
+</ul>
