1bd25b7830a7abe3293762d76ee228d1d51be414
Hakan Çetin <hakan_cetin1205@hotmail.com>
2021-02-13 19:24:21 +0300

    Add files via upload

---
+++ aktiviteler/kurulum/aktivite1.php
@@ -0,0 +1,7 @@
+<h2>DNS'in görevi nedir?</h2>
+<ul style="list-style-type: upper-alpha">
+    <li>İsteği doğru sunucuya yönlendirmek.</li>
+    <li>Bir alan adını bir IP adresine dönüştürmek.</li>
+    <li>Bir istek için bağlantı noktasını dinlemek.</li>
+    <li>İsteği web sunucusuna iletmek.</li>
+</ul>
